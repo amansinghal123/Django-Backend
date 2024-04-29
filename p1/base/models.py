c40ecab8c8d4a15672adb0dcd3c39656bd20511b
amansinghal123 <a.singhaldps@gmail.com>
2024-04-30 00:52:30 +0530

    Added basic Genres

--- p1/base/models.py
+++ p1/base/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
 class Topic(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
     def __str__(self):
