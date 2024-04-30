ca886a0a773d482e338f73ef8180b74bbe6e9ac8
amansinghal123 <a.singhaldps@gmail.com>
2024-05-01 02:07:43 +0530

    Small integration, filtering rooms by Username

--- p1/base/views.py
+++ p1/base/views.py
@@ -59,6 +59,7 @@ def home(request):
         rooms=Room.objects.filter(
             Q(topic__name__icontains=query) |
             Q(name__icontains=query) |
+            Q(host__username__icontains=query) |
             Q(description__icontains=query))
         
     topics=Topic.objects.all()
