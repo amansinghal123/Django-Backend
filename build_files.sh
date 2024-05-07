540942a1270f322259d96832dbe8dd128154c602
amansinghal123 <a.singhaldps@gmail.com>
2024-05-08 00:40:05 +0530

    vercel hosting

---
+++ build_files.sh
@@ -0,0 +1,4 @@
+echo "Build Start"
+python3.12 -m pip install -r requirements.txt
+python3.12 manaage.py collectstatic --noinput --clear
+echo "Build End"
--- p1/p1/settings.py
+++ p1/p1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&!ea_ye8)ac(ep+f+u76(qlt+zlcla1urv&xs@uqps2%8!*bw(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
 AUTH_USER_MODEL = 'base.User'
 # Application definition
@@ -142,3 +142,9 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins (for testing only)
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' , 'static')
