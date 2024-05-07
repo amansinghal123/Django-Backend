3066980698aa1f18d5f6a8eda8b81d13a440b0fc
amansinghal123 <a.singhaldps@gmail.com>
2024-05-08 01:40:14 +0530

    vercel hosting 12

--- p1/p1/settings.py
+++ p1/p1/settings.py
@@ -145,6 +145,4 @@ CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins (for testing only)
 
 import os
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' , 'static')
