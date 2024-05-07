f1aa5a6b80cc396753bd1a32693fe31bfc03d26a
amansinghal123 <a.singhaldps@gmail.com>
2024-05-08 01:30:33 +0530

    vercel hosting 11

--- p1/build_files.sh
+++ p1/build_files.sh
@@ -1,4 +1,4 @@
 echo "Build Start"
 python3.12 -m pip install -r requirements.txt
-python3.12 p1/manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 echo "Build End"
